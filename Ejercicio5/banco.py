15835bf23329bb36c05613bdcc5f26dfa38d6d7b
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-27 17:49:51 +0100

    cambio

--- Ejercicio5/banco.py
+++ Ejercicio5/banco.py
@@ -1,2 +1,20 @@
 class Banco:
+    def __init__(self):
+        self.cuentas = []
     
+    def agregar_cuenta(self, cuenta):
+        self.cuentas.append(cuenta)
+    
+    def buscar_cuenta(self, iban):
+        for cuenta in self.cuentas:
+            if cuenta.iban == iban:
+                print(cuenta)
+            else:
+                print(None)
+    def realizar_transferencias(self, iban_origen, iban_destino, cantidad):
+        origen = self.buscar_cuenta(iban_origen)
+        destino = self.buscar_cuenta(iban_destino)
+
+        if origen.retirar(cantidad) is None:
+            destino.ingresar(cantidad)
+            print(f'Se ha realizado correctamente la transferencia de {cantidad:.2f} de {iban_origen} a {iban_destino}')
