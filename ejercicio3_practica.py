abb5c0876315a139d4db304773adc3970bf5216c
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-27 16:32:57 +0100

    ejercicio 3

---
+++ ejercicio3_practica.py
@@ -0,0 +1,6 @@
+def encontrar_mayor(lista):
+    return max(lista)
+
+Lista = [1,2,3,4,5,6,7,4,6,4,3,342,34,34,62,33425,5432432]
+print(encontrar_mayor(Lista))
+print('La complejidad del algortimo es: O()')
